dd62121743a3ba27391c02e2354b7b218d865a54
小R <xufujian777@gmail.com>
2019-02-27 12:30:10 +0800

    恢复代码至可用浏览器调试的状态

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,9 +1,23 @@
-module.exports = function bestCharge(selectedItems) {
+
+function bestCharge(selectedItems) {
   var title = '============= 订餐明细 =============\n';
   var cutOffRule = '-----------------------------------\n';
   var endRule = '===================================\n';
-
-  var list = '肉夹馍 x 4 = 24元\n';
+  var list = FormatItemList(selectedItems);
   var price = 24;
   return title + list + cutOffRule + '总计：' + price + '元\n' + endRule;
 }
+
+function FormatItemList(selectedItems){
+  //getItemInfoById(1);
+  return '肉夹馍 x 4 = 24元\n';
+}
+
+function getItemInfoById(id){
+    let items = loadAllItems();
+}
+/*
+module.exports = {
+  bestCharge
+ }
+ */
