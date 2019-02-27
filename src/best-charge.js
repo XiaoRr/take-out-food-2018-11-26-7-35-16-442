f15bfa52347cc91d5f40d34e24dc370fa5216601
小R <xufujian777@gmail.com>
2019-02-27 11:25:27 +0800

    重构

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,8 +1,9 @@
 module.exports = function bestCharge(selectedItems) {
+  var title = '============= 订餐明细 =============\n';
+  var cutOffRule = '-----------------------------------\n';
+  var endRule = '===================================\n';
 
-  return `============= 订餐明细 =============
-肉夹馍 x 4 = 24元
------------------------------------
-总计：24元
-===================================`;
+  var list = '肉夹馍 x 4 = 24元\n';
+  var price = 24;
+  return title + list + cutOffRule + '总计：' + price + '元\n' + endRule;
 }
